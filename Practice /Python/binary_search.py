8d1aad9e62012e91e7541f602e2e8c782cc33fc6
venkatasridharsaikalavalapalli <kv.sridharsai@gmail.com>
2021-01-11 23:19:41 +0530

    third commit

---
+++ Practice /Python/binary_search.py
@@ -0,0 +1,25 @@
+def find_the_element(user, ele):
+    right_boundary = len(user) - 1
+    left_boundary = 0
+    while(right_boundary >= left_boundary):
+        middle_element = (left_boundary+(right_boundary-left_boundary) // 2)
+        if user[middle_element] == ele:
+            return middle_element
+        if user[middle_element] > ele:
+            right_boundary = middle_element - 1
+
+        if user[middle_element] < ele:
+            left_boundary = middle_element + 1
+    return 0
+
+user_list = []
+size = int(input("please enter the size of the array : "))
+for i in range(size):
+    user_list.append(int(input("element :")))
+element = int(input("Element to be searched : "))
+
+a = find_the_element(user_list, element)
+if a != 0:
+    print("found at " + str(a))
+else:
+    print("not found")
